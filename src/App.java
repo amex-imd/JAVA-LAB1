1beec5e88bb9267b8f60eeb6d157a68ccfc7b153
amex-imd <ameximd@gmail.com>
2026-02-20 17:39:03 +0300

    Refactor: add Utils class

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Greetings from main!");
         System.out.println("Hello, world!");
+        System.out.println("Happy end!");
     }
 }
